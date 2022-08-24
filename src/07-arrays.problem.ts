00d92baad36ae8aaf6cb4e5657389a886e42a518
Patryk Maron <15141285+patrykmaron@users.noreply.github.com>
2022-08-24 23:25:29 +0100

    Clean comments from excercise 06 found in 07. These are not needed in these files.

--- src/07-arrays.problem.ts
+++ src/07-arrays.problem.ts
@@ -2,12 +2,6 @@ interface User {
   id: number;
   firstName: string;
   lastName: string;
-  /**
-   * How do we ensure that role is only one of:
-   * - 'admin'
-   * - 'user'
-   * - 'super-admin'
-   */
   role: "admin" | "user" | "super-admin";
   posts: Post;
 }
