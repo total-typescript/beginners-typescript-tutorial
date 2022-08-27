8b218eb1887c11ac1ab18d0ba6b6d5fe51149f33
itsAlif <macbookpro@MacBooks-MacBook-Pro.local>
2022-08-27 14:49:06 +0600

    Solved catch block problem

---
+++ src/13-catch-blocks.solution.3.ts
@@ -0,0 +1,15 @@
+import { expect, it } from "vitest";
+
+const tryCatchDemo = (state: "fail" | "succeed") => {
+  try {
+    if (state === "fail") {
+      throw new Error("Failure!");
+    }
+  } catch (e) {
+    return (e as Error).message;
+  }
+};
+
+it("Should return the message when it fails", () => {
+  expect(tryCatchDemo("fail")).toEqual("Failure!");
+});
