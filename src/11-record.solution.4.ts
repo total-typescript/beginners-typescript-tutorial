91177d0febafddb5cbfac9aeb4bce6089c911ef9
Moein <tjex31@yandex.com>
2022-09-08 14:53:18 +0430

    feat: Another solution using type

---
+++ src/11-record.solution.4.ts
@@ -0,0 +1,38 @@
+import { expect, it } from "vitest";
+
+type Cache = Record<string, string>
+
+const createCache = () => {
+  const cache: Cache = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
+
+it("Should add values to the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+
+  expect(cache.cache["123"]).toEqual("Matt");
+});
+
+it("Should remove values to the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+  cache.remove("123");
+
+  expect(cache.cache["123"]).toEqual(undefined);
+});
