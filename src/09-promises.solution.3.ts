e580f15206edc99fc8f1d5dd9153621d62c2658b
Matt Pocock <mattpocockvoice@gmail.com>
2022-09-09 11:35:43 +0100

    Added third solution for 09

---
+++ src/09-promises.solution.3.ts
@@ -0,0 +1,20 @@
+interface LukeSkywalker {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+}
+
+export const fetchLukeSkywalker = async () => {
+  const data: LukeSkywalker = await fetch(
+    "https://swapi.dev/api/people/1"
+  ).then((res) => {
+    return res.json();
+  });
+
+  return data;
+};
