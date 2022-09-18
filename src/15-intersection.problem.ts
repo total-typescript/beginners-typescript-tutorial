e32bffa1fb1f3b8c495db93eecc76be2f4ecd649
Remitekky <remitekky@hotmail.fr>
2022-09-18 16:43:26 +0200

    exo15

--- src/15-intersection.problem.ts
+++ src/15-intersection.problem.ts
@@ -14,7 +14,8 @@ interface Post {
  * How do we type this return statement so it's both
  * User AND { posts: Post[] }
  */
-export const getDefaultUserAndPosts = (): unknown => {
+// Retourne un type User & un array de Post
+export const getDefaultUserAndPosts = (): User & { posts: Post[] } => {
   return {
     id: "1",
     firstName: "Matt",
