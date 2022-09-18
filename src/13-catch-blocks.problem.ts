89b8cb491e2682f33c6a993cc4f32fe660b856b3
Remitekky <remitekky@hotmail.fr>
2022-09-18 16:39:09 +0200

    exo13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -5,7 +5,7 @@ const tryCatchDemo = (state: "fail" | "succeed") => {
     if (state === "fail") {
       throw new Error("Failure!");
     }
-  } catch (e) {
+  } catch (e: any) { // Quand on veut pas un type checking sur une valeur
     return e.message;
   }
 };
