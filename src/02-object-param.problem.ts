f11e20246145024d42d97b0a62c9e123aded6e05
Remitekky <remitekky@hotmail.fr>
2022-09-18 14:27:30 +0200

    exo2

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,24 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+//* On peut définir une interface pour nos objets afin de typer ses propriétés
+// interface Numbers = {
+//   first: number,
+//   second: number
+// }
+
+//* On peut utiliser le raccourcis 'type' pour le même résultat
+// type Numbers = {
+//   first: number,
+//   second: number
+// }
+
+// export const addTwoNumbers = (params: Numbers) => {
+//   return params.first + params.second;
+// };
+
+// On peut sinon typer les propriétés de l'objet reçu en param
+// directement dans l'en-tête de la fonction
+export const addTwoNumbers = (params: { first: number, second: number }) => {
   return params.first + params.second;
 };
 
