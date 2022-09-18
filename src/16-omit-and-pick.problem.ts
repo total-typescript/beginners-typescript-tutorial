66b7c00d7ef1e6dd7cc42d3ec89b92783962f629
Remitekky <remitekky@hotmail.fr>
2022-09-18 16:44:41 +0200

    exo16

--- src/16-omit-and-pick.problem.ts
+++ src/16-omit-and-pick.problem.ts
@@ -10,7 +10,10 @@ interface User {
  * How do we create a new object type with _only_ the
  * firstName and lastName properties of User?
  */
+// Crée un objet à partir d'un type en excluant une propriété
+type MyType = Omit<User, "id">;
 
-type MyType = unknown;
+// Crée un objet à partir d'un type en utilisant certaines de ces propriétés
+// type MyType = Pick<User, "firstName" | "lastName" >;
 
 type tests = [Expect<Equal<MyType, { firstName: string; lastName: string }>>];
