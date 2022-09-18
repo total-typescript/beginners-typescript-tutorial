9cc7deed44e1fb4573290b38798e8c4b0428fce7
Remitekky <remitekky@hotmail.fr>
2022-09-18 16:37:19 +0200

    exo12

--- src/12-typeof-narrowing.problem.ts
+++ src/12-typeof-narrowing.problem.ts
@@ -1,6 +1,15 @@
 import { expect, it } from "vitest";
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+// NARROWING
+// Le param amount est soit un number, soit un objetc avec une propriété de type number
+const coerceAmount = (amount: number | { amount: number }) => {
+  // Pour être sur de toujours renvoyer une valeur exacte quand on fait du NARROWING
+  // on doit conditionner le return selon le type du param
+  if (typeof(amount) === "number") {
+    return amount;
+  }
+  return amount.amount;
+};
 
 it("Should return the amount when passed an object", () => {
   expect(coerceAmount({ amount: 20 })).toEqual(20);
