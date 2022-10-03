009a0633b5bef3b0d62b82ebc7dba82d83efb056
Andrew Davis <atd285@gmail.com>
2022-10-03 13:23:57 -0400

    exercise 01

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -1,6 +1,8 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (a, b) => {
+
+// type annotations fix this issue
+export const addTwoNumbers = (a:number, b:number) => {
   return a + b;
 };
 
@@ -8,3 +10,5 @@ it("Should add the two numbers together", () => {
   expect(addTwoNumbers(2, 4)).toEqual(6);
   expect(addTwoNumbers(10, 10)).toEqual(20);
 });
+
+
