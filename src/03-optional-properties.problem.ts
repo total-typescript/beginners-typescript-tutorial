6e90bfa74025132ffaf48b608094b71ee1d8768a
Andrew Davis <atd285@gmail.com>
2022-10-04 22:51:49 -0400

    more exercises

--- src/03-optional-properties.problem.ts
+++ src/03-optional-properties.problem.ts
@@ -1,6 +1,6 @@
 import { expect, it } from "vitest";
 
-export const getName = (params: { first: string; last: string }) => {
+export const getName = (params: { first: string; last?: string }) => {
   if (params.last) {
     return `${params.first} ${params.last}`;
   }
