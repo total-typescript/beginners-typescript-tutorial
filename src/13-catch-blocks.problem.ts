dbb138e359ce2208c48ccee25831b8d1832c9f5e
Andrew Davis <atd285@gmail.com>
2022-10-12 21:24:36 -0400

    typing errors in try-catch

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -1,12 +1,16 @@
 import { expect, it } from "vitest";
 
+type ErrorMessage = {
+  message: string;
+}
+
 const tryCatchDemo = (state: "fail" | "succeed") => {
   try {
     if (state === "fail") {
       throw new Error("Failure!");
     }
   } catch (e) {
-    return e.message;
+    if (e instanceof Error) return e.message;
   }
 };
 
