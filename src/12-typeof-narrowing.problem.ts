2e7e4ba5d6b696f47518ec5b7f39b38ae3ec90a2
Andrew Davis <atd285@gmail.com>
2022-10-12 21:00:25 -0400

    narrowing down type unions

--- src/12-typeof-narrowing.problem.ts
+++ src/12-typeof-narrowing.problem.ts
@@ -1,6 +1,13 @@
 import { expect, it } from "vitest";
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+const coerceAmount = (amount: number | { amount: number }) => {
+  console.log(typeof amount);
+  if (typeof amount === 'object') {
+   const [obj] = Object.values(amount)
+   return obj;
+  }
+  return amount;
+};
 
 it("Should return the amount when passed an object", () => {
   expect(coerceAmount({ amount: 20 })).toEqual(20);
