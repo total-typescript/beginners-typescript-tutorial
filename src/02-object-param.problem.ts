d2ee0271d9c0591c87e0579852f2838f06c3cd7a
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-11-04 19:36:49 +0100

    init

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,6 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+export const addTwoNumbers = (params: { first: number; second: number }) => {
   return params.first + params.second;
 };
 
@@ -9,13 +9,13 @@ it("Should add the two numbers together", () => {
     addTwoNumbers({
       first: 2,
       second: 4,
-    }),
+    })
   ).toEqual(6);
 
   expect(
     addTwoNumbers({
       first: 10,
       second: 20,
-    }),
+    })
   ).toEqual(30);
 });
