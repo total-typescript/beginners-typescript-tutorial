43f8055064ffadb2b2545bc9e45d26ab1e388491
Erick Mose <mosericko@gmail.com>
2023-09-24 22:30:54 +0300

    feat: finish exercise 4

--- src/04-optional-params.problem.ts
+++ src/04-optional-params.problem.ts
@@ -1,20 +1,20 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
 
-export const getName = (first: string, last: string) => {
-  if (last) {
-    return `${first} ${last}`;
-  }
-  return first;
+export const getName = (first: string, last?: string) => {
+	if (last) {
+		return `${first} ${last}`;
+	}
+	return first;
 };
 
-it("Should work with just the first name", () => {
-  const name = getName("Matt");
+it('Should work with just the first name', () => {
+	const name = getName('Matt');
 
-  expect(name).toEqual("Matt");
+	expect(name).toEqual('Matt');
 });
 
-it("Should work with the first and last name", () => {
-  const name = getName("Matt", "Pocock");
+it('Should work with the first and last name', () => {
+	const name = getName('Matt', 'Pocock');
 
-  expect(name).toEqual("Matt Pocock");
+	expect(name).toEqual('Matt Pocock');
 });
