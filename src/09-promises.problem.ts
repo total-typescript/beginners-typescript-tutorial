c2ab890e22860f1446656b1c32164127f6e024bd
Erick Mose <mosericko@gmail.com>
2023-09-24 22:32:23 +0300

    feat: finish problem 9

--- src/09-promises.problem.ts
+++ src/09-promises.problem.ts
@@ -1,18 +1,18 @@
 interface LukeSkywalker {
-  name: string;
-  height: string;
-  mass: string;
-  hair_color: string;
-  skin_color: string;
-  eye_color: string;
-  birth_year: string;
-  gender: string;
+	name: string;
+	height: string;
+	mass: string;
+	hair_color: string;
+	skin_color: string;
+	eye_color: string;
+	birth_year: string;
+	gender: string;
 }
 
-export const fetchLukeSkywalker = async (): LukeSkywalker => {
-  const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
-    return res.json();
-  });
+export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => {
+	const data = await fetch('https://swapi.dev/api/people/1').then((res) => {
+		return res.json();
+	});
 
-  return data;
+	return data;
 };
