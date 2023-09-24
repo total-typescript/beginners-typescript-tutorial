51679d736eab6ead80c71eb88a4a9773fcbd4bec
Erick Mose <mosericko@gmail.com>
2023-09-24 22:32:08 +0300

    feat: complete problem 8

--- src/08-function-return-type-annotations.problem.ts
+++ src/08-function-return-type-annotations.problem.ts
@@ -1,34 +1,49 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
 
 interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  role: "admin" | "user" | "super-admin";
-  posts: Array<Post>;
+	id: number;
+	firstName: string;
+	lastName: string;
+	role: 'admin' | 'user' | 'super-admin';
+	posts: Array<Post>;
 }
 
 interface Post {
-  id: number;
-  title: string;
+	id: number;
+	title: string;
 }
 
 /**
  * How do we ensure that makeUser ALWAYS
  * returns a user?
  */
-const makeUser = () => {
-  return {};
+const makeUser = (): User => {
+	return {
+		id: 1,
+		firstName: 'Matt',
+		lastName: 'Pocock',
+		role: 'admin',
+		posts: [
+			{
+				id: 1,
+				title: 'How I eat so much cheese',
+			},
+			{
+				id: 2,
+				title: "Why I don't eat more vegetables",
+			},
+		],
+	};
 };
 
-it("Should return a valid user", () => {
-  const user = makeUser();
+it('Should return a valid user', () => {
+	const user = makeUser();
 
-  expect(user.id).toBeTypeOf("number");
-  expect(user.firstName).toBeTypeOf("string");
-  expect(user.lastName).toBeTypeOf("string");
-  expect(user.role).to.be.oneOf(["super-admin", "admin", "user"]);
+	expect(user.id).toBeTypeOf('number');
+	expect(user.firstName).toBeTypeOf('string');
+	expect(user.lastName).toBeTypeOf('string');
+	expect(user.role).to.be.oneOf(['super-admin', 'admin', 'user']);
 
-  expect(user.posts[0].id).toBeTypeOf("number");
-  expect(user.posts[0].title).toBeTypeOf("string");
+	expect(user.posts[0].id).toBeTypeOf('number');
+	expect(user.posts[0].title).toBeTypeOf('string');
 });
