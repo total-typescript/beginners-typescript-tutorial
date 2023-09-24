43c6c2698a6c3a2bda920338bffa46d521df9e26
Erick Mose <mosericko@gmail.com>
2023-09-24 22:31:31 +0300

    feat: complete exercise 6

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -1,20 +1,19 @@
 interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  /**
-   * How do we ensure that role is only one of:
-   * - 'admin'
-   * - 'user'
-   * - 'super-admin'
-   */
-  role: string;
+	id: number;
+	firstName: string;
+	lastName: string;
+	/**
+	 * How do we ensure that role is only one of:
+	 * - 'admin'
+	 * - 'user'
+	 * - 'super-admin'
+	 */
+	role: 'admin' | 'user' | 'super-admin';
 }
 
 export const defaultUser: User = {
-  id: 1,
-  firstName: "Matt",
-  lastName: "Pocock",
-  // @ts-expect-error
-  role: "I_SHOULD_NOT_BE_ALLOWED",
+	id: 1,
+	firstName: 'Matt',
+	lastName: 'Pocock',
+	role: 'admin',
 };
