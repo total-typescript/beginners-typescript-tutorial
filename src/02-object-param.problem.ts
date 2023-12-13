da66e13e8e19b929d0a4cb55487b2defd5e0c318
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-13 11:28:50 +0500

    solve 2

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+type Params = {
+  first: number,
+  second: number, 
+}
+
+export const addTwoNumbers = (params: Params) => {
   return params.first + params.second;
 };
 
