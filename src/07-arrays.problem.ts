48843f47079e69948691baf92d8ba3f2a7497722
yuhcee <egbouchenna001@gmail.com>
2024-10-05 02:15:26 +0100

    :sparkles: complete 07-arrays problem

--- src/07-arrays.problem.ts
+++ src/07-arrays.problem.ts
@@ -1,29 +1,29 @@
 interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  role: "admin" | "user" | "super-admin";
-  posts: Post;
+    id: number;
+    firstName: string;
+    lastName: string;
+    role: 'admin' | 'user' | 'super-admin';
+    posts: Post[];
 }
 
 interface Post {
-  id: number;
-  title: string;
+    id: number;
+    title: string;
 }
 
 export const defaultUser: User = {
-  id: 1,
-  firstName: "Matt",
-  lastName: "Pocock",
-  role: "admin",
-  posts: [
-    {
-      id: 1,
-      title: "How I eat so much cheese",
-    },
-    {
-      id: 2,
-      title: "Why I don't eat more vegetables",
-    },
-  ],
+    id: 1,
+    firstName: 'Matt',
+    lastName: 'Pocock',
+    role: 'admin',
+    posts: [
+        {
+            id: 1,
+            title: 'How I eat so much cheese',
+        },
+        {
+            id: 2,
+            title: "Why I don't eat more vegetables",
+        },
+    ],
 };
