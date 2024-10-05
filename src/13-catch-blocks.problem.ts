e6ea6d8117cfaaca9f8ed170c29f2a0095ca5c3a
yuhcee <egbouchenna001@gmail.com>
2024-10-05 12:05:26 +0100

    :sparkles: complete 13-catch-blocks problem

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -1,15 +1,17 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
 
-const tryCatchDemo = (state: "fail" | "succeed") => {
-  try {
-    if (state === "fail") {
-      throw new Error("Failure!");
+const tryCatchDemo = (state: 'fail' | 'succeed') => {
+    try {
+        if (state === 'fail') {
+            throw new Error('Failure!');
+        }
+    } catch (e) {
+        if (e instanceof Error) {
+            return e.message;
+        }
     }
-  } catch (e) {
-    return e.message;
-  }
 };
 
-it("Should return the message when it fails", () => {
-  expect(tryCatchDemo("fail")).toEqual("Failure!");
+it('Should return the message when it fails', () => {
+    expect(tryCatchDemo('fail')).toEqual('Failure!');
 });
