372a39404a9109c1e60ad5035fdc0eb199295336
yuhcee <egbouchenna001@gmail.com>
2024-10-05 02:10:15 +0100

    :sparkles: complete 05-assigning-types-to-variables problem

--- src/05-assigning-types-to-variables.problem.ts
+++ src/05-assigning-types-to-variables.problem.ts
@@ -11,7 +11,12 @@ interface User {
  * How do we ensure that defaultUser is of type User
  * at THIS LINE - not further down in the code?
  */
-const defaultUser = {};
+const defaultUser : User = {
+  id: 1,
+  firstName: 'Uchenna',
+  lastName: 'Egbo',
+  isAdmin: true
+};
 
 const getUserId = (user: User) => {
   return user.id;
