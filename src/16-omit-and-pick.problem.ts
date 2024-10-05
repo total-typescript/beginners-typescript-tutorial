946f9417b1763377d2376d64e7e28cc513efc76f
yuhcee <egbouchenna001@gmail.com>
2024-10-05 12:39:44 +0100

    :sparkles: complete 16-omit-and-pick problem

--- src/16-omit-and-pick.problem.ts
+++ src/16-omit-and-pick.problem.ts
@@ -1,9 +1,9 @@
-import { Equal, Expect } from "./helpers/type-utils";
+import { Equal, Expect } from './helpers/type-utils';
 
 interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
+    id: string;
+    firstName: string;
+    lastName: string;
 }
 
 /**
@@ -11,6 +11,6 @@ interface User {
  * firstName and lastName properties of User?
  */
 
-type MyType = unknown;
+type MyType = Omit<User, 'id'>;
 
 type tests = [Expect<Equal<MyType, { firstName: string; lastName: string }>>];
