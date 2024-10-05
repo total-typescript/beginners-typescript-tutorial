07aa000175a096c00cbda718d67bade4b08537b0
yuhcee <egbouchenna001@gmail.com>
2024-10-05 16:43:32 +0100

    :sparkles: complete 18-function-type-with-promises problem

--- src/18-function-types-with-promises.problem.ts
+++ src/18-function-types-with-promises.problem.ts
@@ -1,35 +1,32 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
 
 interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
+    id: string;
+    firstName: string;
+    lastName: string;
 }
 
-const createThenGetUser = async (
-  createUser: unknown,
-  getUser: unknown,
-): Promise<User> => {
-  const userId: string = await createUser();
+const createThenGetUser = async (createUser: () => Promise<string>, getUser: (id: string) => Promise<User>): Promise<User> => {
+    const userId: string = await createUser();
 
-  const user = await getUser(userId);
+    const user = await getUser(userId);
 
-  return user;
+    return user;
 };
 
-it("Should create the user, then get them", async () => {
-  const user = await createThenGetUser(
-    async () => "123",
-    async (id) => ({
-      id,
-      firstName: "Matt",
-      lastName: "Pocock",
-    }),
-  );
+it('Should create the user, then get them', async () => {
+    const user = await createThenGetUser(
+        async () => '123',
+        async (id) => ({
+            id,
+            firstName: 'Matt',
+            lastName: 'Pocock',
+        })
+    );
 
-  expect(user).toEqual({
-    id: "123",
-    firstName: "Matt",
-    lastName: "Pocock",
-  });
+    expect(user).toEqual({
+        id: '123',
+        firstName: 'Matt',
+        lastName: 'Pocock',
+    });
 });
