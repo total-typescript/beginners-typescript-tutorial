2ea20d5f1e5bcaef24a128575c1cff0140451179
yuhcee <egbouchenna001@gmail.com>
2024-10-05 11:57:45 +0100

    :sparkles: complete 12-typeof-narrowing problem

--- src/12-typeof-narrowing.problem.ts
+++ src/12-typeof-narrowing.problem.ts
@@ -1,11 +1,17 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+const coerceAmount = (amount: number | { amount: number }) => {
+    if (typeof amount === 'object') {
+        return amount.amount;
+    }
 
-it("Should return the amount when passed an object", () => {
-  expect(coerceAmount({ amount: 20 })).toEqual(20);
+    return amount;
+};
+
+it('Should return the amount when passed an object', () => {
+    expect(coerceAmount({ amount: 20 })).toEqual(20);
 });
 
-it("Should return the amount when passed a number", () => {
-  expect(coerceAmount(20)).toEqual(20);
+it('Should return the amount when passed a number', () => {
+    expect(coerceAmount(20)).toEqual(20);
 });
