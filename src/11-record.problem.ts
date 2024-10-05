b2bae8a027e1d7fcf78f39d49541b6b795905468
yuhcee <egbouchenna001@gmail.com>
2024-10-05 11:57:20 +0100

    :sparkles: complete 11-record problem

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -1,36 +1,40 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
+
+interface ICache {
+    [index: string]: string;
+}
 
 const createCache = () => {
-  const cache = {};
+    const cache: ICache = {};
 
-  const add = (id: string, value: string) => {
-    cache[id] = value;
-  };
+    const add = (id: string, value: string) => {
+        cache[id] = value;
+    };
 
-  const remove = (id: string) => {
-    delete cache[id];
-  };
+    const remove = (id: string) => {
+        delete cache[id];
+    };
 
-  return {
-    cache,
-    add,
-    remove,
-  };
+    return {
+        cache,
+        add,
+        remove,
+    };
 };
 
-it("Should add values to the cache", () => {
-  const cache = createCache();
+it('Should add values to the cache', () => {
+    const cache = createCache();
 
-  cache.add("123", "Matt");
+    cache.add('123', 'Matt');
 
-  expect(cache.cache["123"]).toEqual("Matt");
+    expect(cache.cache['123']).toEqual('Matt');
 });
 
-it("Should remove values from the cache", () => {
-  const cache = createCache();
+it('Should remove values from the cache', () => {
+    const cache = createCache();
 
-  cache.add("123", "Matt");
-  cache.remove("123");
+    cache.add('123', 'Matt');
+    cache.remove('123');
 
-  expect(cache.cache["123"]).toEqual(undefined);
+    expect(cache.cache['123']).toEqual(undefined);
 });
