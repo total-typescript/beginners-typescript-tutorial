bc1db97cf2f5399d06848fe18b86810d44f2021a
yuhcee <egbouchenna001@gmail.com>
2024-10-05 16:20:21 +0100

    :sparkles: complete 17-function-type problem

--- src/17-function-types.problem.ts
+++ src/17-function-types.problem.ts
@@ -1,20 +1,17 @@
-import { Equal, Expect } from "./helpers/type-utils";
+import { Equal, Expect } from './helpers/type-utils';
 
-/**
- * How do we type onFocusChange?
- */
-const addListener = (onFocusChange: unknown) => {
-  window.addEventListener("focus", () => {
-    onFocusChange(true);
-  });
+const addListener = (onFocusChange: (isFocused: boolean) => void) => {
+    window.addEventListener('focus', () => {
+        onFocusChange(true);
+    });
 
-  window.addEventListener("blur", () => {
-    onFocusChange(false);
-  });
+    window.addEventListener('blur', () => {
+        onFocusChange(false);
+    });
 };
 
 addListener((isFocused) => {
-  console.log({ isFocused });
+    console.log({ isFocused });
 
-  type tests = [Expect<Equal<typeof isFocused, boolean>>];
+    type tests = [Expect<Equal<typeof isFocused, boolean>>];
 });
